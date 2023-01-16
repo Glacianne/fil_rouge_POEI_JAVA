87d66c4f393328125e49d07bac6d9bd90e1b2f93
Gael Touquet <gael.touquet@gmail.com>
2023-01-16 12:32:49 +0100

    working createLearnerAccount method

--- Spring/ITraining_API/src/main/java/com/example/itraining_api/controller/LoginController.java
+++ Spring/ITraining_API/src/main/java/com/example/itraining_api/controller/LoginController.java
@@ -1,48 +1,66 @@
 package com.example.itraining_api.controller;
 
-import java.sql.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.itraining_api.entity.LearnerAccount;
+import com.example.itraining_api.entity.TeacherAccount;
 import com.example.itraining_api.entity.UserAccount;
+import com.example.itraining_api.repository.LearnerAccountRepository;
 
 @RestController
-abstract class LoginController {
+public class LoginController {
 
     @Autowired
-    private CrudRepository<UserAccount, Long> crudRepository;
+    private LearnerAccountRepository learnerAccountRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    // @Autowired
+    // private PasswordEncoder passwordEncoder;
 
-    @PostMapping("/createAccount")
-    public ResponseEntity<String> createAccountUser(@RequestBody UserAccount user){
+    // @PostMapping("/createTeacherAccount")
+    // public ResponseEntity<String> createTeaxcherAccount(@RequestBody TeacherAccount teacherAccount) {
+    //     UserAccount savedUser = null;
+    //     ResponseEntity<String> response = null;
+    //     try {
+    //         // String hashPwd = passwordEncoder.encode(teacherAccount.getPassword());
+    //         // teacherAccount.setPassword(hashPwd);
+    //         savedUser = teacherAccountRepository.save(teacherAccount);
+    //         if (savedUser.getId() > 0) {
+    //             response = ResponseEntity.status(HttpStatus.CREATED).body("User has succefully been created");
+    //         }
+    //     } catch (Exception e) {
+    //         response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+    //                 .body("An exception has occured due to " + e.getMessage());
+    //     }
+    //     return response;
+    // }
+
+    @PostMapping("/createLearnerAccount")
+    public ResponseEntity<String> createLearnerAccount(@RequestBody LearnerAccount learnerAccount) {
         UserAccount savedUser = null;
         ResponseEntity<String> response = null;
         try {
-            String hashPwd = passwordEncoder.encode(user.getPassword());
-            user.setPassword(hashPwd);
-            savedUser = crudRepository.save(user);
-            if(savedUser.getId()>0){
-                response = ResponseEntity.status(HttpStatus.CREATED).body("User has succefully been created");
+            // String hashPwd = passwordEncoder.encode(teacherAccount.getPassword());
+            // teacherAccount.setPassword(hashPwd);
+            savedUser = learnerAccountRepository.save(learnerAccount);
+            if (savedUser.getId() > 0) {
+                response = ResponseEntity.status(HttpStatus.CREATED).body("Learner account has succefully been created");
             }
         } catch (Exception e) {
-            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An exception has occured due to " + e.getMessage());
+            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception has occured due to " + e.getMessage());
         }
         return response;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestParam String email, @RequestParam String password){
-        // TODO
-    }
+    // @PostMapping("/login")
+    // public ResponseEntity<String> loginUser(@RequestParam String email,
+    // @RequestParam String password){
+    // // TODO
+    // }
 
 }
