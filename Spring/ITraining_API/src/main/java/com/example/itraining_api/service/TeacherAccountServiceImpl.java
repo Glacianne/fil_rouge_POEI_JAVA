0f786d2de52b23d02410e86ad32101caeae1242c
Admin <perchey.loic@gmail.com>
2023-01-17 09:10:35 +0100

    add TeacherController

---
+++ Spring/ITraining_API/src/main/java/com/example/itraining_api/service/TeacherAccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.itraining_api.service;
+
+import com.example.itraining_api.entity.TeacherAccount;
+import com.example.itraining_api.repository.TeacherAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeacherAccountServiceImpl implements TeacherAccountService{
+    @Autowired
+    private TeacherAccountRepository teacherAccountRepository;
+    @Override
+    public TeacherAccount saveTeacher(TeacherAccount teacherAccount) {
+        return teacherAccountRepository.save(teacherAccount);
+    }
+
+    @Override
+    public List<TeacherAccount> findTeacherAccountList() {
+        return (List<TeacherAccount>)  teacherAccountRepository.findAll();
+    }
+
+    @Override
+    public TeacherAccount updateTeacher(TeacherAccount teacherAccount, int id) {
+       TeacherAccount teacherAccount1 = teacherAccountRepository.findById(id).get();
+       return teacherAccountRepository.save(teacherAccount1);
+    }
+
+    @Override
+    public void deleteTeacherById(int id) {
+        teacherAccountRepository.deleteById(id);
+    }
+}
