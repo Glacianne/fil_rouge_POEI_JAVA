292eeac24536cecb9e7e6e9a568094188a06947b
Anne-Isabelle <anne.isabelle.rene@gmail.com>
2023-01-17 10:49:29 +0100

    CRUD fonctionnel

--- Spring/ITraining_API/src/main/java/com/example/itraining_api/controller/TrainingModuleController.java
+++ Spring/ITraining_API/src/main/java/com/example/itraining_api/controller/TrainingModuleController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TrainingModuleController {
@@ -36,10 +38,63 @@ public class TrainingModuleController {
         return response;
     }
 
-    @GetMapping("/trainingModuleCatalog")
-    public Iterable<Training> findAllTrainingModule(){
-        return trainingModuleRepository.findAll();
+    @DeleteMapping("/deleteTrainingModule/{id}")
+    public ResponseEntity<String> deleteTrainingModule(@PathVariable int id){
+        try{
+            Training training = trainingModuleRepository.findById(id).orElse(null);
+            trainingModuleRepository.delete(training);
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("deleted", Boolean.TRUE);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception has occured due to " + e.getMessage());
+        }
+
+        return ResponseEntity.ok("Formation supprimée");
+
     }
+
+    @PutMapping("/trainingModule/{id}")
+    public ResponseEntity<String> updateTrainingModule(@PathVariable int id, @RequestBody Training trainingDetails){
+        try{
+            Training training = trainingModuleRepository.findById(id).orElse(null);
+           training.setIntitulé(trainingDetails.getIntitulé());
+           training.setStartDate(trainingDetails.getStartDate());
+           training.setEndDate(trainingDetails.getEndDate());
+           training.setListSession(trainingDetails.getListSession());
+           training.setRegisteredLearners(trainingDetails.getRegisteredLearners());
+           trainingModuleRepository.save(training);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception has occured due to " + e.getMessage());
+        }
+        return ResponseEntity.ok("Formation mise à jour");
+    }
+
+    @GetMapping("/trainingModuleCatalog")
+    public ResponseEntity<ArrayList<Training>> findAllTrainingModule(){
+        ArrayList<Training> response = new ArrayList<Training>();
+        try{
+           response = (ArrayList<Training>) trainingModuleRepository.findAll();
+        } catch (Exception e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(response);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/trainingModule/{id}")
+    public ResponseEntity<Training> findTrainingModuleById(@PathVariable int id){
+        Training response = null;
+        try{
+        response = trainingModuleRepository.findById(id).orElse(null);
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+        return ResponseEntity.ok(response);
+}
 
     @PostMapping("/addLearner")
     public ResponseEntity<String> addLearner(@RequestParam int trainingId, @RequestParam int learnerId){
